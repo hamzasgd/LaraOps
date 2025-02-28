534830d4f94ee84a32467d4132fddb64b78c9185
hamzasgd <hamzasgd1@gmail.com>
2025-03-01 04:01:05 +0500

    tinker added in the menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,10 @@
                            class="{{ request()->routeIs('laravelops.env.*') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' }} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                             Environment
                         </a>
+                        <a href="{{ route('laravelops.tinker.index') }}" 
+                           class="{{ request()->routeIs('laravelops.tinker.*') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' }} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                            Tinker
+                        </a>
                         <!-- Add more navigation items here as you build other modules -->
                     </div>
                 </div>
